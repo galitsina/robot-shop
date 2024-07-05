4ad5f279bc50df435d6ffd57468ef4601b389998
Daniella Galitsyna <d.galitsyna@vanlanschotkempen.com>
2024-07-05 15:23:39 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CatalogComponent,
     SiteHeaderComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,30 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { Product } from './product.model';
-import { CartService } from '../cart.service';
 import { ProductService } from './product.service';
+import { CartService } from '../cart/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.css'],
 })
 export class CatalogComponent {
   products: Product[] | undefined;
   filter: string = '';
   // private cartSvc: CartService = inject(CartService);
 
-  constructor (private cartSvc: CartService, private productSvc: ProductService) {
-  }
+  constructor(
+    private cartSvc: CartService,
+    private productSvc: ProductService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.productSvc.getProducts().subscribe(products => this.products = products);
+    this.productSvc
+      .getProducts()
+      .subscribe((products) => (this.products = products));
   }
-  
+
   getFilterProducts() {
-    return this.filter === '' ? this.products : this.products?.filter(product => product.category === this.filter);
+    return this.filter === ''
+      ? this.products
+      : this.products?.filter((product) => product.category === this.filter);
   }
-  
+
   addToCart(product: Product) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
   }
 }
